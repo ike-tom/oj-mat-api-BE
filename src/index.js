9c9284f4ab6df99ce0ca58b41696367aa6da88bc
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-04-04 19:08:09 +0200

    config swagger

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const routes = require('./routes/people.routes');
 const express = require('express');
 const mongoose = require('mongoose');
 const mongoString = process.env.DATABASE_URL;
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
@@ -12,10 +14,31 @@ database.on('error', (error) => {
   console.log(error);
 });
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'oj-mat-api',
+      version: '1.0.0',
+      description: 'Father Matthew Characters API'
+    },
+    servers: [
+      {
+        url: 'https://oj-mat-api.herokuapp.com/api'
+      }
+    ]
+  },
+  apis: ['./routes/*.js']
+};
+
+const specs = swaggerJsDoc(options);
+
 database.once('connected', () => {
   console.log('Database Connected');
 });
 const app = express();
+
+app.use('/api/doc', swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(express.json());
 app.use('/api', routes);
