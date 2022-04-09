2da0e145f255897857542bb29d99e15cecce86a9
Tomasz Ikert <80857044+ike-tom@users.noreply.github.com>
2022-04-09 12:52:02 +0200

    typo

--- src/routes/people.routes.js
+++ src/routes/people.routes.js
@@ -19,7 +19,7 @@ router.get('/people', async (req, res) => {
 router.get('/people/:id', async (req, res) => {
   try {
     const data = await Person.findById(req.params.id);
-    if (data === null) res.json('Character with this id does not exists.');
+    if (data === null) res.json('A character with this id does not exist.');
     res.json(data);
   } catch (error) {
     res.status(error.status || 500).send({
